17694161d2f1a2993f0441058cd3e09bcf249f5a
조예슬 <joyeseul@joyeseul-ui-MacBookPro.local>
2022-10-14 21:41:37 +0900

    [STYLE] 주석 추가

--- seminar2/Advanced/src/api/user.ts
+++ seminar2/Advanced/src/api/user.ts
@@ -1,7 +1,7 @@
 // user.ts
 import { Request, Response } from "express";
 
-const getUser = (req: Request, res: Response) => {
+const getUser = (req: Request, res: Response) => { // 각 요청/응답에 대한 로직을 함수로 작성
 
     return res.status(200).json({
         status: 200,
@@ -13,4 +13,4 @@ const getUser = (req: Request, res: Response) => {
     });
 };
 
-export default getUser;
+export default getUser; // 모듈화 해준 뒤 export 해서 userRouter 함수에서 사용
--- seminar2/Advanced/src/router/userRouter.ts
+++ seminar2/Advanced/src/router/userRouter.ts
@@ -1,6 +1,6 @@
 
 import express, { Router } from "express";
-import getUser from "../api/user"; 
+import getUser from "../api/user"; // 모듈 import해서 깔꼼하게 사용하기
 
 const router: Router = express.Router();
 
